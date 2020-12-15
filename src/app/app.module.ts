216a5e95e665bc6050957f4e5ef889176339c549
Andrei Romanciuc <56957259+andreiromanciuc@users.noreply.github.com>
2020-12-15 22:00:52 +0200

    added customer and partner pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { PartnerPageComponent } from './partner-page/partner-page.component';
+import { CustomerPageComponent } from './customer-page/customer-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    LoginPageComponent,
+    PartnerPageComponent,
+    CustomerPageComponent
   ],
   imports: [
     BrowserModule,
